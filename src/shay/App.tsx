969541ff4c6f41e28d3e86f7143ad11cac74eb08
Yonatan Lavie <yonatan.lavie89@gmail.com>
2021-10-22 11:59:33 +0300

    Done! :)

---
+++ src/shay/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import BingMap from "./shay/BingMap";
+
+const App = () => {
+    return (
+        <div className="App">
+            App
+
+        </div>
+    );
+};
+
+export default App;
